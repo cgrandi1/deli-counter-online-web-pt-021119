52872e441d8e2b4deb699d0226c0e3c4985c2569
astronomical-form-6016 <carltondgrandison@gmail.com>
2019-03-10 21:01:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,7 @@ def line(katz_deli)
        puts "The line is currently empty."  
        
      else 
-      current_line = "The line is currently: " 
+      current_line = "The line is currently:" 
       katz_deli.each.with_index(1) do |name, index|
          current_line << "#{index}. #{name}"
      end 
