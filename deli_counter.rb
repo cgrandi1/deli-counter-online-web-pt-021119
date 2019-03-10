03479839baff6e8fe7b164ecec927c0ed3e69d44
astronomical-form-6016 <carltondgrandison@gmail.com>
2019-03-10 20:41:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,7 +9,7 @@ def line(katz_deli)
        
      else 
        katz_deli.each.with_index(1) do 
-         |name, index| puts "The line is currently,  "
+         |name, index| puts "The line is currently"
      
      
      else def take_a_number
