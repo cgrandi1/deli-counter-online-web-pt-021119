146c44711af2520887457e4214ea3215a6d1f026
astronomical-form-6016 <carltondgrandison@gmail.com>
2019-03-10 19:47:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,14 @@
 
 require 'pry'
 
+  katz_deli = []
+  
 def line(katz_deli)
      if katz_deli = []
        puts "The line is currently empty."  
        
-     else
-       current_line = "The line is currently:"
+     else def take_a_number (name, line)
+       current_line = "The line is currently: "
        katz_deli.each.with_index(1) do |name, index|
        current_line.unshift  "#{index}. #{name}."
      end 
