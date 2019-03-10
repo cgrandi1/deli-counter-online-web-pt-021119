08b886eb45931b047dfbd2b5c26fdb62470afd78
astronomical-form-6016 <carltondgrandison@gmail.com>
2019-03-10 19:50:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(katz_deli)
      if katz_deli = []
        puts "The line is currently empty."  
        
-     else def take_a_number (name, line)
+     else def line
        
      end 
      
